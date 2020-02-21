de8d6ab163f3653976f9a586f1a664b44eece6b5
Akylbekov Bakyt <mr.akylbekovbakyt@gmail.com>
2020-02-21 16:13:56 +0600

    edit preloader

--- js/script.js
+++ js/script.js
@@ -1,14 +1,16 @@
 $('.mobile-btn').on('click',  function() {
     $('.mobile-btn').toggleClass('show-mobile-btn');
     $('.menu').toggleClass('show-menu');
-    $('.menu_item').toggleClass('show-menu_item')
+    $('.menu_item').toggleClass('show-menu_item');
+    $('body').toggleClass('lock');
 });
 
 $('.menu nav a').on('click',  function() {
     $('.mobile-btn').removeClass('show-mobile-btn');
     $('.menu').removeClass('show-menu');
-    $('.menu_item').removeClass('show-menu_item')
+    $('.menu_item').removeClass('show-menu_item');
 });
+
 
 
 $(window).scroll(function () {
@@ -18,3 +20,19 @@ $(window).scroll(function () {
         $('header').removeClass('header-fixed');
     }
 });
+
+function preloader() {
+    $(()=> {
+        setInterval(()=>{
+            let p = $('.preloader');
+
+            p.css('opacity', 0);
+
+            setInterval(
+                () => p.remove(),
+                parseInt(p.css('--duration')) * 1000
+            );
+        }, 1000);
+    });
+}
+setInterval(() => preloader(), 1000);
